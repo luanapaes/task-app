de1a436919ba9782401a5fac06fc897bf45b38c6
Luana Paes <luanafranca598@gmail.com>
2025-02-04 20:43:38 -0300

    feat(add-task-button): cria AddTaskButton componente com template, styles, e tests

---
+++ src/app/shared/add-task-button/add-task-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-task-button.component.html',
+  styleUrl: './add-task-button.component.scss'
+})
+export class AddTaskButtonComponent {
+
+}
