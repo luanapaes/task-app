289cfabe89cad02ae1d7eec2a43800886e9f0a26
Luana Paes <luanafranca598@gmail.com>
2025-02-04 22:24:59 -0300

    feat(home): adiciona exibição de tarefas no HomeComponent com estilo e estrutura

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { AddTaskButtonComponent } from '../../shared/add-task-button/add-task-button.component';
 import { StatusCardComponent } from '../../shared/status-card/status-card.component';
+import { TaskComponent } from '../../shared/task/task.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent, AddTaskButtonComponent,
-    StatusCardComponent
+    StatusCardComponent, TaskComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
