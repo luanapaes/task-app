83c23f27aedab4092d37b3b4a663d94fe8f46b09
Luana Paes <luanafranca598@gmail.com>
2025-02-04 23:53:57 -0300

    feat(environment): add configurações development e production environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { Environment } from "../app/shared/interfaces/environment-interface";
+
+export const environment: Environment = {
+    env: 'prod',
+    apiTask: 'https://pptjtclcnajollxqfunx.supabase.co/rest/v1/tasks',
+    apiKEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwdGp0Y2xjbmFqb2xseHFmdW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MTM5MTgsImV4cCI6MjA1NDE4OTkxOH0.7XadGsQf6SA7dV9WTTny-PI8t8_UXkUfIEQWdKQUA3A'
+};
