4ceeed7ca1bee9bd98f65acdbe67aacbf2740cf8
Luana Paes <luanafranca598@gmail.com>
2025-02-04 23:58:22 -0300

    feat(home): integra TaskService e implementa recuperação de tasks com signal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { AddTaskButtonComponent } from '../../shared/add-task-button/add-task-button.component';
 import { StatusCardComponent } from '../../shared/status-card/status-card.component';
 import { TaskComponent } from '../../shared/task/task.component';
+import { Task } from '../../shared/interfaces/task-interface';
+import { TaskService } from '../../shared/services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent, AddTaskButtonComponent,
-    StatusCardComponent, TaskComponent
+    StatusCardComponent, TaskComponent, CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  #taskService = inject(TaskService)
+
+  public tasks = signal<null | Task[]>([]);
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getTask()
+  }
+
+  public task$ = this.#taskService.getAll();
+
+  getTask(){
+    this.task$.subscribe(
+      {
+        next: (next) => {
+          this.tasks.set(next);
+        },
+        error: (error) => console.log(error),
+        complete: () => console.log("Completed!")
+      }
+    )
+  }
 
 }
