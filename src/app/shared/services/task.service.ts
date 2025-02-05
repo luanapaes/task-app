92b6dd880a7cd4ee8e81cb29348082d8c8b0d727
Luana Paes <luanafranca598@gmail.com>
2025-02-04 23:56:56 -0300

    feat(task): implementarTaskService para gerenciar tarefas com integração de API

---
+++ src/app/shared/services/task.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Task } from '../interfaces/task-interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { Observable, shareReplay } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  urlAPI = signal(environment.apiTask);
+  apiKey = signal(environment.apiKEY)
+  httpClient = inject(HttpClient)
+  
+  constructor() { }
+
+  getAll(): Observable<Task[]>{
+    const headers = { 
+      'apikey': this.apiKey(), 
+      'Authorization': `Bearer ${this.apiKey()}` 
+    };
+
+    return this.httpClient.get<Task[]>(this.urlAPI(), { headers }).pipe(
+      shareReplay()
+    );
+  }
+}
