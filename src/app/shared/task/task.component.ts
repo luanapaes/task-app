6a44b11cfea93cf152d00dd50dbc2f9ff3fc425d
Luana Paes <luanafranca598@gmail.com>
2025-02-04 23:57:35 -0300

    refactor(task): remove onInit não utilizado do código

--- src/app/shared/task/task.component.ts
+++ src/app/shared/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-task',
@@ -12,10 +13,4 @@ export class TaskComponent{
   @Input() description: string = '';
   @Input() createAt: string = '';
   @Input() status: boolean = true;
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    console.log(this.status)
-  }
 }
