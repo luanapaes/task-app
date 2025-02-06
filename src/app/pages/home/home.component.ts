42eb82e77983256fe32465eba19c828543b40c44
Luana Paes <luanafranca598@gmail.com>
2025-02-05 21:19:24 -0300

    feat(HomeComponent): adiciona 'handleTaskSelected' que recebe o id da task e abre o DialogTask

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,9 @@ import { TaskComponent } from '../../shared/task/task.component';
 import { Task } from '../../shared/interfaces/task-interface';
 import { TaskService } from '../../shared/services/task.service';
 import { CommonModule, DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogTaskComponent } from '../../shared/dialog-task/dialog-task.component';
 
 @Component({
   selector: 'app-home',
@@ -21,11 +24,28 @@ export class HomeComponent {
   #taskService = inject(TaskService)
 
   public tasks = signal<null | Task[]>([]);
+  router = inject(Router)
+
+  constructor(public dialog: MatDialog){}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.getTask()
+  }
+
+  handleTaskSelected(taskId: number) {
+    this.router.navigate(['task', taskId]).then(() => {
+      const dialogRef = this.dialog.open(DialogTaskComponent, {
+        data: { id: String(taskId)}
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if(result){
+          this.router.navigate([''])
+        }
+      })
+    })
   }
 
   public task$ = this.#taskService.getAll();
