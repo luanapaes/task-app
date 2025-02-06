630c801677f79e47dd083cff800cdd4a887f2cb8
Luana Paes <luanafranca598@gmail.com>
2025-02-05 21:17:12 -0300

    feat(TaskComponent): adiciona interação com o DialogComponent

--- src/app/shared/task/task.component.ts
+++ src/app/shared/task/task.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogTaskComponent } from '../dialog-task/dialog-task.component';
+import { CustomTaskData } from '../interfaces/custom-task-dialog.interface';
 
 
 @Component({
@@ -8,9 +11,44 @@ import { Component, Input } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
-export class TaskComponent{
+export class TaskComponent {
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input() createAt: string = '';
+  @Input() createAt: null | string = '';
   @Input() status: boolean = true;
+
+  readonly task = signal<CustomTaskData>({
+    id: '',
+    title: '',
+    description: '',
+    status: false
+  })
+  
+  readonly dialog = inject(MatDialog);
+
+  @Input() taskInput: any; // recebe a task do HomeComponent
+  @Output() taskSelected = new EventEmitter<number>(); // emite o ID da task para o home
+
+  selectTask() {
+    this.taskSelected.emit(this.taskInput.id); // Emite o ID da tarefa para o componente pai
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogTaskComponent, {
+      width: '500px',
+      height: '700px',
+      data: { 
+        titleTaskDialog: this.task().title,
+        descriptionTaskDialog: this.task().description,
+        statusTaskDialog: this.task().status
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.task.set(result);
+      }
+    });
+  }
 }
