4203bdfc85baa5a13ad72838305aa912d290991c
Luana Paes <luanafranca598@gmail.com>
2025-02-05 21:26:22 -0300

    feat(route): adiciona rota dinâmica task/:id com resolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { getTask } from './shared/resolvers/getTask.resolver';
 
 export const routes: Routes = [
     {
         path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'task/:id',
+        resolve: {
+            task: getTask
+        },
         component: HomeComponent
     }
 ];
